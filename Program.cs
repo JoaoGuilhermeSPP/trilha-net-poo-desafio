1a00a5b15c586a9ff6760bfc155ed3ebdad255e7
JoaoGuilhermeSPP <joaoguipinheiro@gmail.com>
2025-08-04 21:34:14 -0300

    Integrando cell

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone iphone = new Iphone(numero: "998887777", modelo: "T5-0", imei: "444", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instalando Whatsapp...");
+
+Smartphone nokia = new Nokia(numero: "977774444", modelo: "XEE", imei: "5555", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instalando Linkedin...");
